48c8eabb472013be48d5dae4b44a553cf05df470
APereiraScripts <168227076+AndresP2006@users.noreply.github.com>
2025-10-28 17:01:03 -0500

    Correcion de la trabla de solicitud de visitas

--- frontend/src/layouts/user/RegistrosUsuario.tsx
+++ frontend/src/layouts/user/RegistrosUsuario.tsx
@@ -1,3 +1,4 @@
+import SolicitudVisitas from "../../components/tablas/SolicitudVisitas";
 import Icons from "../../utils/Icons";
 
 function RegistrosUsuario() {
@@ -35,30 +36,13 @@ function RegistrosUsuario() {
       <br />
       <div>
         <div className="flex justify-between items-center mb-6">
-        <h2 className="text-xl font-bold">Permisos de Visitas</h2>
-        <span className="material-icons cursor-pointer text-3xl border-2 bg-gray-300 ">
-          {Icons.Habrir()}
-        </span>
-      </div>
-
-        <div className="mt-2 max-h-100 overflow-y-auto rounded-lg border border-b-gray-300">
-          <table className="min-w-full border-collapse text-center">
-            <thead className="bg-gray-100 sticky top-0">
-              <tr>
-                <th className="p-2 font-bold">Documento</th>
-                <th className="p-2 font-bold">Nombre y Apellido</th>
-                <th className="p-2 font-bold">Teléfono</th>
-                <th className="p-2 font-bold">Estado</th>
-                <th className="p-2 font-bold">Información</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border-t">
-                {/* <td className="p-2">hola</td> */}
-              </tr>
-            </tbody>
-          </table>
+          <h2 className="text-xl font-bold">Permisos de Visitas</h2>
+          <span className="material-icons cursor-pointer text-3xl border-2 bg-gray-300 ">
+            {Icons.Habrir()}
+          </span>
         </div>
+
+        <SolicitudVisitas></SolicitudVisitas>
       </div>
     </div>
   );
